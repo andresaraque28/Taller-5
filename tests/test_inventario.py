45e18af06c55626b965bb293112e1033be2c9f61
IngAngelicaPira <angelicapira.95@gmail.com>
2026-09-02 22:00:59 -0500

    Se induce a un test rojo
    se realiza la modificacion de del test_inventario para inducir al error en los test y en la subida del commit, se deja comentado el valor original para restaura

--- tests/test_inventario.py
+++ tests/test_inventario.py
@@ -11,17 +11,16 @@ def test_dias_normales():
 
 def test_sin_ventas_dura_infinito():
     # sin ventas, devolvemos -1 (señal de "no se acaba")
-    assert dias_de_inventario(50, 0) == -1.0
+    assert dias_de_inventario(50, 0) == 1.0 # modificacion de -1 al 1 para inducir al
 
 
 def test_necesita_reposicion_true():
     # 20 unidades, 10 por día -> 2 días < 7 -> sí necesita
-    assert necesita_reposicion(20, 10) is True
-
+    assert necesita_reposicion(20, 10) is False # valor original True
 
 def test_no_necesita_reposicion():
     # 100 unidades, 10 por día -> 10 días > 7 -> no necesita
-    assert necesita_reposicion(100, 10) is False
+    assert necesita_reposicion(100, 10) is True # Valor original False
 
 
 def test_stock_negativo_lanza_error():
